e74bcc258b00992fb313b7f167d9aa2d7dbd30cb
tayler <taylerallen6@gmail.com>
2024-06-04 16:35:24 -0500

    small update

--- swaybg/.config/swaybg/launch_wallpaper.sh
+++ swaybg/.config/swaybg/launch_wallpaper.sh
@@ -1,5 +1,5 @@
 #swaybg --image ~/dotfiles/misc_files/wallpapers/nord_mountains.png
 
-swaybg --image ~/dotfiles/misc_files/wallpapers/firewatch/1267208.png
+swaybg --image ~/dotfiles/misc_files/wallpapers/11356491.png
 
 
