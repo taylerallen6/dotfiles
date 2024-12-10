55467f8d835f29a39118a256b82bcd3179d2bc9d
Tayler Allen <taylerallen6@gmail.com>
2024-12-10 17:30:28 -0600

    updating install commands

--- misc_files/install_commands_old.sh
+++ misc_files/install_commands_old.sh
@@ -6,50 +6,27 @@
 ### alacritty themes
 # https://github.com/alacritty/alacritty-theme
 
-### USEFUL FIRST INSTALLS
-sudo pacman -S git
-sudo pacman -S curl
-sudo pacman -S xdg-user-dirs
+### copy tuigreet to /etc/tuigreet
+sudo cp tuigreet_config.toml /etc/greetd/config.toml
 
-### ADD USER DIRECTORIES
-# commands after installs:
-xdg-user-dirs-update
-
-### INSTALL YAY
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-### RUST
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-### REBOS
-cargo install rebos
-rebos setup
-rebos config init
-
-### NVM / NPM / NODE.JS
+### nvm / npm / node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 nvm install 20.0 # installs node v20.0
 
-### TUIGREET
-# commands after installs:
-systemctl enable greetd.service
-sudo cp tuigreet_config.toml /etc/greetd/config.toml
+### harden firefox
 
-### INSTALL OH-MY-BASH
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+### rofi
 
-### BLUETOOTH
-# commands after installs:
-# start service
-sudo systemctl start bluetooth
-sudo systemctl enable bluetooth
+### rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup component add rls
+rustup component add rust-analyzer
+rustup component add clippy
+rustup component add rust-std
 
 # SCREEN SHARING
 # https://gist.github.com/brunoanc/2dea6ddf6974ba4e5d26c3139ffb7580
 # https://wiki.archlinux.org/title/PipeWire
-# commands after installs:
 # Add this line to your hyprland.conf file.
 exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 # Do NOT install Grimblast screenshot tool! It will mess up screen sharing!
@@ -61,6 +38,11 @@ cp -r /usr/share/pipewire ~/.config/pipewire # copy default pipewire configs (?)
 sudo systemctl start cups
 sudo systemctl enable cups
 sudo usermod -a -G lp {your-username}
+
+### BLUETOOTH
+# start service
+sudo systemctl start bluetooth
+sudo systemctl enable bluetooth
 
 ### EWW
 # https://elkowar.github.io/eww/
